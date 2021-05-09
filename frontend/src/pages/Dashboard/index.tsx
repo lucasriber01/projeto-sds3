c5c4ab6e7dfdf77c03a187707d8049de5173c8ff
Jehan Lucas <lucasarago.df@gmail.com>
2021-05-08 22:46:16 -0300

    Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ImgDsDark from 'assets/img/ds-dark.svg'
+import NavBar from 'components/NavBar';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+
+
+const Dashboard = () => {
+    return (
+        <>
+        <NavBar/>
+        <div className="container">
+          <h1 className="text-primary py-3">DashBoard Teste</h1>
+  
+  
+          <div className="row px-3" >
+            <div className="col-sm-6">
+              <h5 className="text-center  text-secondary">Taxa de Sucesso(%)</h5>
+              <BarChart/>
+            </div>
+  
+  
+            <div className="col-sm-6">
+              <h5 className="text-center  text-secondary">Todas as Vendas</h5>
+              <DonutChart/>
+            </div>
+          </div>
+  
+          
+          
+          <div className="py-3">
+            <h2 className= "text-primary"> Todas as vendas  </h2>
+          </div>
+          <DataTable/>
+        </div>
+       <Footer/>
+      </>
+    );
+}
+
+export default Dashboard;
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImgDsDark from 'assets/img/ds-dark.svg'
+
+
+const Home = () => {
+    return (
+        <div >
+            <h1>Página Home</h1>
+        </div>
+    );
+}
+
+export default Home;
